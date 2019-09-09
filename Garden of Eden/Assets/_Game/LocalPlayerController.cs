e3b72c88587916bfc9554b46b0eb1e7de6b1fb19
Daan de Bruijn <ddebruijn9@gmail.com>
2019-09-09 10:37:05 +0200

    Update 0.3
    Networking

---
+++ Garden of Eden/Assets/_Game/LocalPlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LocalPlayerController : NetworkBehaviour
+{
+    public Camera localCamera;
+
+    public float movementSpeed;
+
+    Vector3 localPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            Destroy(localCamera);
+        }
+        else
+        {
+            // Taking care of the camera when another player joins.
+            if (localCamera.tag != "MainCamera")
+            {
+                localCamera.tag = "MainCamera";
+                localCamera.enabled = true;
+            }
+        }
+
+        localPosition.x = Input.GetAxis("Horizontal");
+        localPosition.z = Input.GetAxis("Vertical");
+
+        transform.position += localPosition * movementSpeed * Time.deltaTime;
+    }
+}
