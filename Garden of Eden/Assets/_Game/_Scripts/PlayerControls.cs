e9de53e9dc942e6792f87a7a47372d9c42d75949
Daan de Bruijn <ddebruijn9@gmail.com>
2019-09-09 16:49:34 +0200

    Update 1.1
    Networking Bare Minimum Fished

---
+++ Garden of Eden/Assets/_Game/_Scripts/PlayerControls.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float movementSpeed;
+
+    Vector3 localPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        localPosition.x = Input.GetAxis("Horizontal");
+        localPosition.z = Input.GetAxis("Vertical");
+
+        transform.position += localPosition * movementSpeed * Time.deltaTime;
+    }
+}
---
+++ Garden of Eden/Assets/_Game/_Scripts/PlayerSetup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] nonLocalComponents;
+
+    Camera lobbyCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < nonLocalComponents.Length; i++)
+            {
+                nonLocalComponents[i].enabled = false;
+            }
+        }
+        else
+        {
+            lobbyCamera = Camera.main;
+            lobbyCamera.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (lobbyCamera != null)
+        {
+            lobbyCamera.gameObject.SetActive(true);
+        }
+    }
+}
