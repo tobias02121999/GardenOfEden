cfff42de6cd387b83e52e12667cdbdc36ecfaf59
tobias02121999 <hedgefoggames@outlook.com>
2019-09-16 16:15:47 +0200

    Update 3.0

--- Garden of Eden/Assets/FollowPivot.cs
+++ Garden of Eden/Assets/FollowPivot.cs
@@ -7,7 +7,7 @@ public class FollowPivot : MonoBehaviour
     // Initialize the public variables
     public Transform target;
     public Transform reference;
-    public float speed;
+    public float speed, rotationSpeed;
 
     // Initialize the private variables
     Rigidbody rb;
@@ -16,6 +16,7 @@ public class FollowPivot : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = Mathf.Infinity;
     }
 
     // Update is called once per frame
@@ -23,12 +24,44 @@ public class FollowPivot : MonoBehaviour
     {
         Reference(); // Rotate the reference transform towards the target
         Move(); // Move towards the target pivot
+        Rotate(); // Rotate towards the target pivot
     }
 
     // Rotate the reference transform towards the target
     void Reference()
     {
         reference.LookAt(target);
+    }
+
+    // Calculate the required torque to equal the rotation to a target rotation
+    Vector3 ComputeTorque(Transform targetTransform, Quaternion desiredRotation)
+    {
+        //q will rotate from our current rotation to desired rotation
+        Quaternion q = desiredRotation * Quaternion.Inverse(targetTransform.rotation);
+
+        //convert to angle axis representation so we can do math with angular velocity
+        Vector3 x;
+        float xMag;
+        q.ToAngleAxis(out xMag, out x);
+        x.Normalize();
+
+        var rb = targetTransform.GetComponent<Rigidbody>();
+
+        //w is the angular velocity we need to achieve
+        Vector3 w = x * xMag * Mathf.Deg2Rad / Time.fixedDeltaTime;
+        w -= rb.angularVelocity;
+
+        //to multiply with inertia tensor local then rotationTensor coords
+        Vector3 wl = targetTransform.InverseTransformDirection(w);
+        Vector3 Tl;
+        Vector3 wll = wl;
+
+        wll = rb.inertiaTensorRotation * wll;
+        wll.Scale(rb.inertiaTensor);
+        Tl = Quaternion.Inverse(rb.inertiaTensorRotation) * wll;
+        Vector3 T = targetTransform.TransformDirection(Tl);
+
+        return T;
     }
 
     // Move towards the target pivot
@@ -38,6 +71,14 @@ public class FollowPivot : MonoBehaviour
         var step = dist * speed;
 
         rb.velocity = reference.forward * step;
-        rb.rotation = target.rotation;
+    }
+
+    // Rotate towards the target pivot
+    void Rotate()
+    {
+        var angle = Vector3.Angle(target.forward, transform.forward);
+        var step = angle * rotationSpeed;
+
+        rb.AddTorque(ComputeTorque(transform, target.rotation) * step);
     }
 }
