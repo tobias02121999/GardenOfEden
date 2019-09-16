ca2bd7033d807f6f1da79230bbc34c34d6a26df2
tobias02121999 <hedgefoggames@outlook.com>
2019-09-16 15:04:22 +0200

    Update 2.7

---
+++ Garden of Eden/Assets/FollowPivot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPivot : MonoBehaviour
+{
+    // Initialize the public variables
+    public Transform target;
+    public float speed;
+
+    // Initialize the private variables
+    Rigidbody rb;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(); // Move towards the target pivot
+    }
+
+    // Move towards the target pivot
+    void Move()
+    {
+        var dist = Vector3.Distance(transform.position, target.position);
+        var step = dist * speed;
+
+        rb.position = Vector3.Lerp(transform.position, target.position, step);
+        rb.rotation = target.rotation;
+    }
+}
