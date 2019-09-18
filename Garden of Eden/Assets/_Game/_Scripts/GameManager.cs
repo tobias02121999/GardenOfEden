70f9a390f1aa3492cc057a4755e0d16b35b376e7
Daan de Bruijn <ddebruijn9@gmail.com>
2019-09-18 10:50:57 +0200

    PandaBT 0.2

---
+++ Garden of Eden/Assets/_Game/_Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager instance = null;
+    private void Awake()
+    {
+        if (instance != this)
+        {
+            Destroy(instance);
+        }
+    }
+    #endregion
+
+    public GameObject cube;
+
+    public static List<GameObject> fearObjects = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var go = Instantiate(cube, new Vector3(3, 0, 0), Quaternion.identity);
+            fearObjects.Add(go);
+        }
+    }
+}
