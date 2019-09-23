81d5448b91521601496b6c05ba6df1b9260ec5b5
Tobias Brugel <hedgefoggames@outlook.com>
2019-09-23 21:23:01 +0200

    Update 4.8

---
+++ Garden of Eden/Assets/WaveGeneration.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveGeneration : MonoBehaviour
+{
+    // Initialize the public variables
+    public float scale, speed, strength, height;
+
+    // Initialize the private variables
+    Mesh mesh;
+    Vector3[] baseHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (baseHeight == null)
+            baseHeight = mesh.vertices;
+
+        Vector3[] vertices = new Vector3[baseHeight.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 vertex = baseHeight[i];
+
+            vertex.y += Mathf.Sin(Time.time * speed + baseHeight[i].x + baseHeight[i].y + baseHeight[i].z) * scale;
+            vertex.y += Mathf.PerlinNoise(baseHeight[i].x, baseHeight[i].y + Mathf.Sin(Time.time * .1f)) * strength;
+
+            vertices[i] = vertex;
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
+    }
+}
---
+++ Garden of Eden/Assets/WaveGeneration1.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveGeneration1 : MonoBehaviour
+{
+    // Initialize the public variables
+    public int width, height, depth, scale;
+    public float speedX, speedY;
+
+    // Initialize the private variables
+    float offsetX, offsetY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+
+        offsetX += speedX * Time.deltaTime;
+        offsetY += speedY * Time.deltaTime;
+    }
+
+    // Generate terrain data
+    TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, GenerateHeights());
+
+        return terrainData;
+    }
+
+    // Generate terrain heights
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y);
+            }
+        }
+
+        return heights;
+    }
+
+    // Calculate the height
+    float CalculateHeight(int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
