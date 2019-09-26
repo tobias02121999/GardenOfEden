4c593cc1a3ce47d04f271b2b77eb255f9ebe9db2
tobias02121999 <hedgefoggames@outlook.com>
2019-09-26 16:48:10 +0200

    Update 5.2

--- Garden of Eden/Assets/Sun.cs
+++ Garden of Eden/Assets/Sun.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Sun : MonoBehaviour
 {
     // Initialize the public variables
-    public float dayNightSpeed;
+    public float dayNightSpeed, densityDay, densityNight, ambientDay, ambientNight;
     public Color waterColorDay, waterColorNight;
     public Color auraColorDay, auraColorNight;
+    public Color mistColorDay, mistColorNight;
     public Material waterMaterial, skyMaterial;
+    public ParticleSystem mist;
     public AuraAPI.Aura aura;
 
     // Initialize the private variables
@@ -35,24 +37,36 @@ public class Sun : MonoBehaviour
         if (rotation >= 360f)
             rotation = 0f;
 
-        Color waterColor, auraColor;
+        Color waterColor, auraColor, mistColor;
+        float density, ambient;
 
         if (rotation <= 180f)
         {
             var index = rotation / 180f;
             waterColor = Color.Lerp(waterColorDay, waterColorNight, index);
             auraColor = Color.Lerp(auraColorDay, auraColorNight, index);
+            density = Mathf.Lerp(densityDay, densityNight, index);
+            ambient = Mathf.Lerp(ambientDay, ambientNight, index);
+            mistColor = Color.Lerp(mistColorDay, mistColorNight, index);
         }
         else
         {
             var index = (rotation - 180f) / 180f;
             waterColor = Color.Lerp(waterColorNight, waterColorDay, index);
             auraColor = Color.Lerp(auraColorNight, auraColorDay, index);
+            density = Mathf.Lerp(densityNight, densityDay, index);
+            ambient = Mathf.Lerp(ambientNight, ambientDay, index);
+            mistColor = Color.Lerp(mistColorNight, mistColorDay, index);
         }
 
         waterMaterial.SetColor("_DepthGradientDeep", waterColor);
         skyMaterial.color = waterColor;
 
         aura.frustum.settings.color = auraColor;
+        aura.frustum.settings.density = density;
+        aura.frustum.settings.colorStrength = ambient;
+
+        var main = mist.main;
+        main.startColor = mistColor;
     }
 }
