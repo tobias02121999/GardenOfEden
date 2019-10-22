8eea224119b17e4bb6be33dcd00ef904be3e46bf
tobias02121999 <hedgefoggames@outlook.com>
2019-10-22 14:04:41 +0200

    Update 8.6

---
+++ Garden of Eden/Assets/_Game/_Scripts/HumanSync.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HumanSync : NetworkBehaviour
+{
+    // Initialize the public variables
+    [HideInInspector]
+    public NetworkPlayers networkPlayers;
+
+    [HideInInspector]
+    public NetworkIdentity identity;
+
+    // Initialize the private variables
+    bool serverInControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        networkPlayers = FindObjectOfType<NetworkPlayers>();
+        identity = GetComponent<NetworkIdentity>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        identity.localPlayerAuthority = serverInControl;
+    }
+
+    // 
+    void OnTriggerStay(Collider other)
+    {
+        var obj = other.GetComponentInParent<PlayerController>().gameObject;
+        if (obj == networkPlayers.otherPlayer)
+            serverInControl = false;
+        else
+            serverInControl = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        serverInControl = true;
+    }
+}
---
+++ Garden of Eden/Assets/_Game/_Scripts/NetworkPlayers.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkPlayers : MonoBehaviour
+{
+    // Initialize the public variables
+    [HideInInspector]
+    public GameObject localPlayer, otherPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
