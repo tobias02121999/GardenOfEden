7e5ecf99ad77c17c028a0e07918a5078f56d2f17
Tobias Brugel <hedgefoggames@outlook.com>
2019-10-24 09:06:47 +0200

    Update 10.0

---
+++ Network Testing/Network Testing/Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Initialize the private variables
+    Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.backgroundColor = Random.ColorHSV();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Network Testing/Network Testing/Assets/PlayerSync.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class PlayerSync : NetworkBehaviour
+{
+    // Initialize the public variables
+    public GameObject cube;
+    public Text cubeInfo1, cubeInfo2;
+
+    // Initialize the private variables
+    bool cubeSpawned, authoritySet;
+    GameObject cubeInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            var cam = GetComponent<Camera>();
+            cam.enabled = false;
+
+            var player = GetComponent<Player>();
+            player.enabled = false;
+
+            var canvas = GetComponentInChildren<Canvas>().gameObject;
+            canvas.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            if (Input.GetButtonDown("Fire1") && !cubeSpawned)
+            {
+                CmdSpawnCube();
+                cubeSpawned = true;
+            }
+
+            if (Input.GetButtonDown("Fire2") && ! authoritySet)
+            {
+                CmdSetClientAuthority();
+                authoritySet = true;
+            }
+
+            if (cubeSpawned)
+                cubeInfo1.text = "Cube Spawned: True";
+            else
+                cubeInfo1.text = "Cube Spawned: False";
+
+            if (authoritySet)
+                cubeInfo2.text = "Authority: Client";
+            else
+                cubeInfo2.text = "Authority: Host";
+        }
+    }
+
+    [Command]
+    void CmdSpawnCube()
+    {
+        cubeInstance = (GameObject)Instantiate(cube, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        //NetworkServer.SpawnWithClientAuthority(obj, connectionToClient);
+        NetworkServer.Spawn(cubeInstance);
+    }
+
+    [Command]
+    void CmdSetClientAuthority()
+    {
+        var identity = cubeInstance.GetComponent<NetworkIdentity>();
+        identity.AssignClientAuthority(connectionToClient);
+    }
+}
