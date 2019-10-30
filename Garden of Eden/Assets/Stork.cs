4bcfe483506c79109e9f181ff63a2d89ac63f249
Sergi van Ravenswaay <sergi.vanravenswaay@student.hku.nl>
2019-10-30 11:30:33 +0100

    Update 1.2.9

---
+++ Garden of Eden/Assets/Stork.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Stork : NetworkBehaviour
+{
+    // Initialize the public variables
+    public GameObject humanPrefab;
+    public Transform humanParent;
+    public GameObject human;
+    public Rigidbody humanHips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CmdSpawnHuman();
+
+        humanHips = human.transform.Find("mixamorig:Hips").GetComponent<Rigidbody>();
+        humanHips.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ;
+        human.GetComponent<RagdollAnimator>().impactRecovery = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Spawn the human
+    [Command]
+    void CmdSpawnHuman()
+    {
+        human = Instantiate(humanPrefab, humanParent.transform.position, Quaternion.identity);
+        human.transform.parent = humanParent;
+
+        NetworkServer.Spawn(human);
+    }
+}
---
+++ Garden of Eden/Assets/StorkDropzone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StorkDropzone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Check for storks dropping humans
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Stork"))
+        {
+            var stork = other.GetComponentInParent<Stork>();
+
+            stork.humanHips.constraints = RigidbodyConstraints.None;
+            stork.human.GetComponent<RagdollAnimator>().impactRecovery = 1f;
+
+            stork.human.transform.parent = null;
+        }
+    }
+}
