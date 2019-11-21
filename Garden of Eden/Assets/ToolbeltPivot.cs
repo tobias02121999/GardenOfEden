d583aeaaaf6ebb4c9c72f75b1ad8b7caed6e314c
Sergi van Ravenswaay <sergi.vanravenswaay@student.hku.nl>
2019-11-21 14:41:44 +0100

    Update 1.4.8

---
+++ Garden of Eden/Assets/ToolbeltPivot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolbeltPivot : MonoBehaviour
+{
+    // Initialize the public variables
+    public Transform headTransform;
+
+    // Initialize the private variables
+    float startPosY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = headTransform.position;
+        transform.rotation = Quaternion.Euler(0f, headTransform.localEulerAngles.y, 0f);
+    }
+}
