fca8beaf730e85fd2435eb717a78ac5e563598f2
Sergi van Ravenswaay <sergi.vanravenswaay@student.hku.nl>
2019-11-23 16:03:59 +0100

    Update 1.5.2

---
+++ Garden of Eden/Assets/ToolBelt.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolBelt : MonoBehaviour
+{
+    // Initialize the public variables
+    public Transform cameraTransform;
+    public float flexibility, turnability;
+    public float distToHead;
+    public float moveSpd, turnSpd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowCam(); // Follow the camera transform
+        RotateCam(); // Rotate towards the camera transform
+    }
+
+    // Follow the camera transform
+    void FollowCam()
+    {
+        var t = new Vector3();
+        var h = cameraTransform.position;
+        var tf = flexibility;
+        var l = distToHead;
+
+        t.x = Mathf.Clamp(transform.position.x, h.x - tf, h.x + tf);
+        t.z = Mathf.Clamp(transform.position.z, h.z - tf, h.z + tf);
+        t.y = h.y - l;
+
+        var step = moveSpd * Time.deltaTime;
+
+        transform.position = Vector3.Slerp(transform.position, t, step);
+    }
+
+    // Rotate towards the camera transform
+    void RotateCam()
+    {
+        var r = transform.localEulerAngles.y;
+        var tr = cameraTransform.localEulerAngles.y;
+        var tf = turnability;
+
+        var rotY = Mathf.Clamp(r, tr - tf, tr + tf);
+
+        var step = turnSpd * Time.deltaTime;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, rotY, 0f), step);
+    }
+}
---
+++ Garden of Eden/Assets/ToolbeltSlot.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolbeltSlot : MonoBehaviour
+{
+    // Initialize the public variables
+    public GameObject[] toolMesh;
+    public int toolID;
+
+    // Initialize the private variables
+    bool hasInteracted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DisplayMesh(); // Display the correct mesh
+    }
+
+    // Set the tool ID
+    void OnTriggerEnter(Collider other)
+    {
+        if (!hasInteracted)
+        {
+            var handTag = other.GetComponentInParent<FollowPivot>().tag;
+            int handType;
+
+            if (handTag == "HandLeft")
+                handType = 0;
+            else
+                handType = 1;
+
+            var ID = GetToolID(other.tag);
+            var inv = other.GetComponentInParent<PlayerInventory>();
+            SwitchTool(ID, inv, handType);
+
+            hasInteracted = true;
+        }
+    }
+
+    // Reset the interaction bool
+    void OnTriggerExit(Collider other)
+    {
+        hasInteracted = false;
+    }
+
+    // Switch tools
+    void SwitchTool(int targetID, PlayerInventory targetInv, int handType)
+    {
+        int newID;
+
+        if (handType == 0)
+        {
+            newID = targetInv.currentToolL;
+            targetInv.currentToolL = toolID;
+        }
+        else
+        {
+            newID = targetInv.currentToolR;
+            targetInv.currentToolR = toolID;
+        }
+
+        toolID = newID;
+    }
+
+    // Display the correct mesh
+    void DisplayMesh()
+    {
+        var length = toolMesh.Length;
+        for (var i = 0; i < length; i++)
+        {
+            var isActive = (i == toolID);
+            toolMesh[i].SetActive(isActive);
+        }
+    }
+
+    // Convert a tool name into a tool ID
+    int GetToolID(string name)
+    {
+        var ID = 0;
+
+        switch (name)
+        {
+            case "Hand":
+                ID = 0;
+                break;
+
+            case "Paintbrush":
+                ID = 1;
+                break;
+
+            case "Hammer":
+                ID = 2;
+                break;
+
+            case "Lookingglass":
+                ID = 3;
+                break;
+        }
+
+        return ID;
+    }
+}
