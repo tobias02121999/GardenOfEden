1c711b26093f74a50ae29df0fdd9fe79f4d6ce77
tobias02121999 <hedgefoggames@outlook.com>
2019-12-17 19:21:28 +0100

    Update 1.8.8

---
+++ Garden of Eden/Assets/Cloud.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    // Initialize the public variables
+    public float cloudDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cloudDuration--;
+
+        if (cloudDuration <= 0f)
+            Destroy(this.gameObject);
+    }
+}
