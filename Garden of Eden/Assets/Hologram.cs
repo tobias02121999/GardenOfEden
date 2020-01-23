ed13d74e305e23a3592f293204d5abb51e8ca2b9
loljoch <loljoch@gmail.com>
2020-01-23 13:18:13 +0100

    Post-Mortem Build 3

--- Garden of Eden/Assets/Hologram.cs
+++ Garden of Eden/Assets/Hologram.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Hologram : MonoBehaviour
+public class Hologram : NetworkBehaviour
 {
     // Initialize the public variables
     public GameObject spawnPrefab;
@@ -30,7 +31,8 @@ public class Hologram : MonoBehaviour
 
         if (hasCollided && inventory.paint >= paintCost && spawnPrefab != null)
         {
-            Instantiate(spawnPrefab, transform.position, transform.rotation);
+            CmdSpawnObject(isHome, isFarm, NetworkPlayers.Instance.localPlayer.GetComponent<PlayerSetup>().teamID);
+
             paintRenderer.lineRenderer.positionCount = 0;
             inventory.paint -= paintCost;
             Destroy(this.gameObject);
@@ -48,5 +50,31 @@ public class Hologram : MonoBehaviour
     {
         if (other.CompareTag("Hand"))
             hasCollided = true;
+    }
+
+    [Command]
+    void CmdSpawnObject(bool _isHome, bool _isFarm, int teamID)
+    {
+        var obj = Instantiate(spawnPrefab, transform.position, transform.rotation); Instantiate(spawnPrefab);
+
+        if (_isHome)
+        {
+            if (teamID == 0)
+                GameManager.Instance.teamOneHomes.Add(obj);
+
+            if (teamID == 1)
+                GameManager.Instance.teamTwoHomes.Add(obj);
+        }
+
+        if (_isFarm)
+        {
+            if (teamID == 0)
+                GameManager.Instance.teamOneFarms.Add(obj);
+
+            if (teamID == 1)
+                GameManager.Instance.teamTwoFarms.Add(obj);
+        }
+
+        NetworkServer.Spawn(obj);
     }
 }
