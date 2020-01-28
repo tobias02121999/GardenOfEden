01d3af20e39d9da738b6b1b2e22e1d6c5ebcd5b0
tobias02121999 <hedgefoggames@outlook.com>
2020-01-28 15:16:38 +0100

    Update 2.3.5

--- Garden of Eden/Assets/Cloud.cs
+++ Garden of Eden/Assets/Cloud.cs
@@ -7,20 +7,41 @@ public class Cloud : NetworkBehaviour
 {
     // Initialize the public variables
     public float cloudDuration;
+    public GameObject target;
+    public bool hasTarget;
+    public NetworkIdentity identity;
+
+    // Initialize the private variables
+    CloudSetup setup;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        setup = GetComponent<CloudSetup>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        cloudDuration--;
+        if (setup.teamID == NetworkPlayers.Instance.localPlayer.GetComponent<PlayerSetup>().teamID)
+        {
+            cloudDuration--;
 
-        if (cloudDuration <= 0f)
-            Destroy(this.gameObject);
+            if (cloudDuration <= 0f)
+                Destroy(this.gameObject);
+
+            if (target != null)
+            {
+                var ID = target.GetComponentInParent<NetworkIdentity>().netId;
+                identity = target.GetComponentInParent<NetworkIdentity>();
+
+                if (target.CompareTag("Farm"))
+                    CmdFarmWet(ID, hasTarget);
+
+                if (target.CompareTag("Sapling"))
+                    CmdSaplingWet(ID, hasTarget);
+            }
+        }
     }
 
     // Tell the target object if its wet or not
--- Garden of Eden/Assets/RainCollider.cs
+++ Garden of Eden/Assets/RainCollider.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 
-public class RainCollider : NetworkBehaviour
+public class RainCollider : MonoBehaviour
 {
+    // Initialize the public variables
+    public Cloud cloud;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,26 +28,33 @@ public class RainCollider : NetworkBehaviour
     // Tell the object it's colliding with that it's getting rained on
     void OnTriggerStay(Collider other)
     {
-        var cloud = GetComponentInParent<Cloud>();
-        var ID = other.GetComponentInParent<NetworkIdentity>().netId;
+        if (other.CompareTag("Farm") || other.CompareTag("Sapling"))
+        {
+            cloud.target = other.gameObject;
+            cloud.hasTarget = true;
+        }
 
+        /*
         if (other.CompareTag("Farm"))
             cloud.CmdFarmWet(ID, true);
 
         if (other.CompareTag("Sapling"))
             cloud.CmdSaplingWet(ID, true);
+            */
     }
 
     // Tell the object it's colliding with that it's not getting rained on
     void OnTriggerExit(Collider other)
     {
-        var cloud = GetComponentInParent<Cloud>();
-        var ID = other.GetComponentInParent<NetworkIdentity>().netId;
+        if (other.CompareTag("Farm") || other.CompareTag("Sapling"))
+            cloud.hasTarget = false;
 
+        /*
         if (other.CompareTag("Farm"))
             cloud.CmdFarmWet(ID, false);
 
         if (other.CompareTag("Sapling"))
             cloud.CmdSaplingWet(ID, false);
+            */
     }
 }
